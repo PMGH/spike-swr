ee267583ec4c544c1460ba5dbf8fd677f1bab7b2
Peter McCready <peter.mccready@ooni.com>
2023-01-18 14:30:49 +0000

    refactor and add comments

--- src/pages/beers/index.tsx
+++ src/pages/beers/index.tsx
@@ -1,49 +1,58 @@
 import { swrFetcher } from "../../../swr/utils";
-import useSwr, { SWRConfig } from 'swr'
+import useSwr, { SWRConfig } from 'swr';
 
 type Beer = {
   id: string;
   name: string;
-};
-
+}
 type BeersPageProps = {
   fallback: Beer[]
 }
+type FeaturedBeerProps = {
+  id: string;
+}
 
-const BeersList = () => {
+const StaticBeersList = () => {
   const { data: staticData, error } = useSwr<Beer[]>('/api/beers', swrFetcher);
   console.log({ staticData, error })
-  return (
-    <ul>
-      {staticData && staticData.map(beer => <li key={beer.id}>{beer.name}</li>)}
-    </ul>
-  )
+  if (staticData) {
+    return (
+      <ul>
+        {staticData.map(beer => <li key={beer.id}>{beer.name}</li>)}
+      </ul>
+    )
+  }
+  return <p>Beers not found.</p>
+}
+
+const DynamicFeaturedBeer = ({ id }: FeaturedBeerProps) => {
+  const SINGLE_BEER_URL = `https://api.punkapi.com/v2/beers/${id}`;
+  const { data: dynamicData, error, isLoading } = useSwr<Beer[]>(SINGLE_BEER_URL, swrFetcher);
+  console.log({ dynamicData, error, isLoading  })
+
+  if (isLoading) return <p>Loading...</p>
+  if (dynamicData) return <p>Featured Beer: {dynamicData[0].name}</p>
+  return <p>Featured beer not found.</p>
 }
 
 const BeersPage = ({ fallback }: BeersPageProps) => {
-  const { data: dynamicData, error } = useSwr<Beer[]>('https://api.punkapi.com/v2/beers/1', swrFetcher);
-  console.log({ dynamicData, error, fallback  })
+  console.log({ fallback })
 
-  const renderFeaturedBeer = () => {
-    if (dynamicData?.length && dynamicData[0]?.name) {
-      return <p>Featured Beer: {dynamicData[0].name}</p>
-    }
-
-    return <p>Loading...</p>
-  }
-
+  // SWRConfig provides the fallback data to the useSwr hook
+  // which enables the useSwr hook in StaticBeersList to provide the data
   return (
     <SWRConfig value={{ fallback }}>
       <h1>Beers Page</h1>
-      {renderFeaturedBeer()}
+      <DynamicFeaturedBeer id="1" />
       <h3>Beers</h3>
-      <BeersList />
+      <StaticBeersList />
     </SWRConfig>
   )
 }
 
 export default BeersPage
 
+// Data fetched server-side during yarn build (or in yarn dev)
 export const getStaticProps = async () => {
   const ALL_BEERS_URL = 'https://api.punkapi.com/v2/beers';
   const staticBeers = await fetch(ALL_BEERS_URL).then(res => res.json());
